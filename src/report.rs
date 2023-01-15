494581592be5928b4605de23be93e40c6251d4ce
mathkruger <ms-kruger@hotmail.com>
2023-01-15 09:36:15 -0300

    reporting hour bank

--- src/report.rs
+++ src/report.rs
@@ -1,7 +1,7 @@
 use chrono::{DateTime, Utc};
 use dateparser::parse;
 use json::{self, JsonValue};
-use owo_colors::OwoColorize;
+use owo_colors::{OwoColorize, Style};
 
 use crate::utils::format_time;
 
@@ -12,14 +12,17 @@ pub fn get_report(data: &JsonValue) {
     }
     
     println!(
-        "{0: <10} | {1: <10} | {2: <15} | {3: <15} | {4: <10} | {5: <15}",
+        "{0: <10} | {1: <10} | {2: <15} | {3: <15} | {4: <10} | {5: <18} | {6: <10}",
         "Data".bold(),
         "Início".bold(),
         "Início - almoço".bold(),
         "Fim - almoço".bold(),
         "Fim".bold(),
-        "Total de horas".bold()
+        "Horas trabalhadas".bold(),
+        "Saldo".bold()
     );
+
+    let mut total_hour_bank = 0;
 
     for item in data.members() {
         let worked_seconds = get_worked_hours_from_day(
@@ -41,14 +44,12 @@ pub fn get_report(data: &JsonValue) {
             },
         );
 
-        let total_minutes = worked_seconds / 60;
-
-        let seconds = worked_seconds % 60;
-        let hours = total_minutes / 60;
-        let minutes = total_minutes % 60;
+        let day_hour_bank = worked_seconds - 28800;
+        let (hours, minutes, seconds) = seconds_to_hour(worked_seconds.abs());
+        let (hour_bank_formatted, hour_bank_color) = get_formatted_bank_time(day_hour_bank);
 
         println!(
-            "{0: <10} | {1: <10} | {2: <15} | {3: <15} | {4: <10} | {5: <15}",
+            "{0: <10} | {1: <10} | {2: <15} | {3: <15} | {4: <10} | {5: <18} | {6: <10}",
             item["date"],
             if item["startTime"].is_empty() {
                 "00:00:00"
@@ -71,10 +72,22 @@ pub fn get_report(data: &JsonValue) {
                 item["endTime"].as_str().unwrap()
             },
             format_time(hours as u32, minutes as u32, seconds as u32),
+            hour_bank_formatted.style(hour_bank_color),
         );
+
+        total_hour_bank += day_hour_bank;
     }
 
-    println!("");
+    let separator = "-";
+
+    let (total_hour_bank_formatted, style) = get_formatted_bank_time(total_hour_bank);
+
+    println!(
+        "{0: <72}---{1: <18} | {2: <10}",
+        separator.repeat(72),
+        "-------Saldo total",
+        total_hour_bank_formatted.style(style)
+    );
 }
 
 pub fn get_worked_hours_from_day(
@@ -91,14 +104,48 @@ pub fn get_worked_hours_from_day(
     let worked_without_lunch = end_time_parsed.time() - start_time_parsed.time();
     let lunch_duration = lunch_end_time_parsed.time() - lunch_start_time_parsed.time();
 
-    return worked_without_lunch.num_seconds() - lunch_duration.num_seconds();
+    return worked_without_lunch.num_seconds() - lunch_duration.num_seconds()
 }
 
 fn get_time_or_last(time_to_parse: &str, last_time: &DateTime<Utc>) -> DateTime<Utc> {
     let parsed = match parse(time_to_parse) {
         Ok(value) => value,
-        Err(_) => last_time.clone(),
+        Err(_) => last_time.clone()
     };
 
     parsed
+}
+
+fn seconds_to_hour(raw_seconds: i64) -> (i64, i64, i64) {
+    if raw_seconds == 0 {
+        return (0, 0, 0)
+    }
+
+    let total_minutes = raw_seconds / 60;
+    let seconds = raw_seconds % 60;
+    let hours = total_minutes / 60;
+    let minutes = total_minutes % 60;
+
+    (hours, minutes, seconds)
+}
+
+fn get_formatted_bank_time(raw_seconds: i64) -> (std::string::String, Style) {
+    let (bank_hours, bank_minutes, bank_seconds) = seconds_to_hour(raw_seconds.abs());
+
+    let hour_bank_type: &str = if raw_seconds < 0 {
+        "-"
+    } else {
+        " "
+    };
+
+    let hour_bank_formatted = hour_bank_type.to_owned() +
+        &format_time(bank_hours as u32, bank_minutes as u32, bank_seconds as u32);
+
+    let hour_bank_color = if hour_bank_type == "-" {
+        Style::new().red().bold()
+    } else {
+        Style::new().green().bold()
+    };
+
+    (hour_bank_formatted, hour_bank_color)
 }
