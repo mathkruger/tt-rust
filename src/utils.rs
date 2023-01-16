80377835f2e0cf26c23b9b574913417e4debc4c5
mathkruger <ms-kruger@hotmail.com>
2023-01-15 21:50:45 -0300

    using the generic format method to format the date and time

--- src/utils.rs
+++ src/utils.rs
@@ -2,48 +2,21 @@ use owo_colors::OwoColorize;
 
 pub fn format_date(year: u32, month: u32, day: u32) -> String {
     let separator = "-".to_string();
-    let year = year.to_string();
-
-    let month = if month < 10 {
-        "0".to_string() + &month.to_string()
-    } else {
-        month.to_string()
-    };
-
-    let day = if day < 10 {
-        "0".to_string() + &day.to_string()
-    } else {
-        day.to_string()
-    };
-
-    return year + &separator + &month + &separator + &day;
+    let items = vec![year, month, day];
+    let date_formatted = format(items, separator);
+    
+    date_formatted
 }
 
 pub fn format_time(hour: u32, minutes: u32, seconds: u32) -> String {
     let separator = ":".to_string();
-
-    let hour = if hour < 10 {
-        "0".to_string() + &hour.to_string()
-    } else {
-        hour.to_string()
-    };
-
-    let minutes = if minutes < 10 {
-        "0".to_string() + &minutes.to_string()
-    } else {
-        minutes.to_string()
-    };
-
-    let seconds = if seconds < 10 {
-        "0".to_string() + &seconds.to_string()
-    } else {
-        seconds.to_string()
-    };
-
-    return hour + &separator + &minutes + &separator + &seconds;
+    let items = vec![hour, minutes, seconds];
+    let time_formatted = format(items, separator);
+    
+    time_formatted
 }
 
-fn format(mut items: Vec<u32>, separator: String) -> String {
+fn format(items: Vec<u32>, separator: String) -> String {
     let mut strings: Vec<String> = Vec::new();
     
     for el in items {
@@ -56,7 +29,7 @@ fn format(mut items: Vec<u32>, separator: String) -> String {
         strings.push(parsed);
     }
     
-    return strings.join(separator).to_string()
+    return strings.join(&separator).to_string()
 }
 
 pub fn application_banner() {
